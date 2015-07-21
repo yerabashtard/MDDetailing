ddc50e3ab047f4c5500e0b52a7d8ad3b215dec95
Justin Cheema <justin.cheema@blackboard.com>
2015-07-20 22:09:21 -0600

    Project on hold until Mark tells me what he wants to dogit status

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -84,17 +84,17 @@ angular.module('MD Detailing Application.controllers', [])
   	{
   		id: 0,
   		blurb: "1. Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
-  		image: "img/Mark1.jpg",
+  		image: "img/DeckGarden1.jpg",
   	},
   	{
   		id: 1,
   		blurb: "2. Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
-  		image: "img/MarkFamily1.jpg",
+  		image: "img/DeckGarden2.jpg",
   	},
   	{
   		id: 2,
   		blurb: "3. Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
-  		image: "img/MarkFamily2.jpg",
+  		image: "img/DeckGarden3.jpg",
   	}];
 
   }])
